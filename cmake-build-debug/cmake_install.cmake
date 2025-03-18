9ef8ab79d7d7d4c87355eff2340194e6e39b2d68
Laurentiu Vlaicu <vlaicul@outlook.com>
2025-03-18 02:24:29 +0200

    Nothing

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Laurentiu/SDA_2024-2025/Labor/DSA_Bag_DLL_L2
+# Install script for directory: C:/Laurentiu files/Development/SDA 2024_2025_Lab2_BagDLL
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Laurentiu/SDA_2024-2025/Labor/DSA_Bag_DLL_L2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Laurentiu files/Development/SDA 2024_2025_Lab2_BagDLL/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
