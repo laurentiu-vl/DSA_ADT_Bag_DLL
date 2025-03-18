430ad4b43512a3b30a098e14704a9b9ee47b542a
Laurentiu Vlaicu <vlaicul@outlook.com>
2025-03-18 02:44:19 +0200

    Iterator impvr.

--- BagIterator.cpp
+++ BagIterator.cpp
@@ -12,25 +12,23 @@ using namespace std;
 
 BagIterator::BagIterator(const Bag& c): bag(c)
 {
-    //TODO - Implementation
 
     current = bag.head;
+
 }
 
 void BagIterator::first() {
-    //TODO - Implementation
 
     current = bag.head;
+
 }
 
 
 void BagIterator::next() {
-    //TODO - Implementation
 
     if (!valid()) {
         throw std::out_of_range("Iterator out of range");
     }
-
     if (current->next == nullptr) {
         current = nullptr; //e ok?
     }
@@ -40,23 +38,17 @@ void BagIterator::next() {
 
 }
 
-
 bool BagIterator::valid() const {
-    //TODO - Implementation
 
-    return current != nullptr; //?????????????
+    return current != nullptr;
+
 }
 
-
-
-TElem BagIterator::getCurrent() const
-{
-    //TODO - Implementation
+TElem BagIterator::getCurrent() const {
 
     if (!valid()) {
         throw std::out_of_range("Iterator out of range");
     }
-    return current->data.first; //e ok doar elementul TElem? sau trebuie si frecventa? TElem BagIterator...
+    return current->data.first && current->data.second; //e ok doar elementul TElem? sau trebuie si frecventa? TElem BagIterator...
 
-    //return NULL_TELEM
 }
