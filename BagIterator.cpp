883d564589b5ffffe303e405f1533fd6081a7e2f
Laurentiu Vlaicu <vlaicul@outlook.com>
2025-03-25 02:58:57 +0200

    next(), first() updated

--- BagIterator.cpp
+++ BagIterator.cpp
@@ -17,62 +17,46 @@ BagIterator::BagIterator(const Bag &c): bag(c) {
 
     if (current != nullptr) {
         freq = current->data.second;
+        currentFreq = freq;
     }
-
-    // if (size()) {
-    //
-    // }
 }
 
 void BagIterator::first() {
     current = bag.head;
+    currentFreq = freq; //de verificat
 }
 
 void BagIterator::next() {
-    // if (!valid()) {
-    //     throw std::out_of_range("Iterator out of range");
-    // }
-    // if (current->next == nullptr) {
-    //     current = nullptr; //e ok?
-    // } else {
-    //
-    //     current = current->next;
-    // }
 
     if (!valid()) {
         throw std::out_of_range("Iterator out of range");
     }
-
-    freq--;
+    currentFreq--;
     //cout << "Frequency is " << current->data.second - freq << " for element: " << current->data.first << endl;
 
-    if (freq == 0) {
+    if (currentFreq == 0) {
         current = current->next;
         if (current != nullptr) {
-            freq = current->data.second;
+            currentFreq = current->data.second;
         }
-
-
-
-
-        // if (current->next != nullptr) {
-        //     freq = current->data.second;
-        // }
     }
-    // else {
-    //     freq--;
-    // }
 
+    // freq--;
+    // //cout << "Frequency is " << current->data.second - freq << " for element: " << current->data.first << endl;
+    //
+    // if (freq == 0) {
+    //     current = current->next;
+    //     if (current != nullptr) {
+    //         freq = current->data.second;
+    //     }
+    // }
 }
 
 bool BagIterator::valid() const {
-    //return current != nullptr;
+
     if (current == nullptr) {
         return false;
     }
-    // if (current->next == nullptr) {
-    //     return false;
-    // }
     return true;
 }
 
@@ -81,12 +65,6 @@ TElem BagIterator::getCurrent() const {
         return current->data.first;
     }
     throw std::out_of_range("Iterator out of range");
-
-    // if (!valid()) {
-    //     throw std::out_of_range("Iterator out of range");
-    // }
-    // return current->data.first; //pt ca TElem e declarat doar ca si element typedef .h
-    //&& current->data.second; //e ok doar elementul TElem? sau trebuie si frecventa? TElem BagIterator...
 }
 
 int BagIterator::getFreq() const {
